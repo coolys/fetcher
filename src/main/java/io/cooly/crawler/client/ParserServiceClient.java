81ae45ac342c98b9999737747cce8054d2c9946b
Hung Nguyen <nguyendanghung2008@gmail.com>
2019-05-09 03:33:37 +0100

    update micro service client

--- src/main/java/io/cooly/crawler/client/ParserServiceClient.java
+++ src/main/java/io/cooly/crawler/client/ParserServiceClient.java
@@ -4,11 +4,16 @@ package io.cooly.crawler.client;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
 @AuthorizedFeignClient(name = "parser")
-public interface WebPageClient {
+public interface ParserServiceClient {
+    
     @RequestMapping("/api/web-pages")
     List<WebPage> getFetches();
+    
+    @PostMapping("api/web-pages")
+    WebPage createWebPage(WebPage webPage);
 }
